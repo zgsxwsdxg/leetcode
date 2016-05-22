7d2ed68a25a76200eac7fb323b11f2093b4afa01
zhangzhen <1099905725@qq.com>
2016-05-22 14:31:32 +0800

    3.1

--- Longest_Substring_Without_Repeating_Characters.cpp
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -2,9 +2,7 @@
 class Solution {
 	public:
 		int lengthOfLongestSubstring(string s) {
-
-			int pos[256] = {
-				0};
+			int pos[256] ={0};
 			int left = 0;
 			int right = 0;
 			int len = 0;
@@ -20,6 +18,24 @@ class Solution {
 				pos[s[i]] = 1;
 				right = i;
 				len = std::max(len, right - left + 1);
+			}
+			return len;
+		}
+};
+
+//版本二，记录跳转位置
+class Solution {
+	public:
+		int lengthOfLongestSubstring(string s) {
+			int pos[256] = {0};
+			int left = 0;
+			int len = 0;
+			for(size_t i = 0; i < s.size(); ++i){
+				if(pos[s[i]] > left){
+					left = pos[s[i]];
+				}
+				pos[s[i]] = i + 1;
+				len = std::max(len, static_cast<int>(i) - left + 1);
 			}
 			return len;
 		}
