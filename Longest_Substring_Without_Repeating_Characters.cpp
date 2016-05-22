b6fb59521305aa35d938478821442f30103e89f0
zhangzhen <1099905725@qq.com>
2016-05-22 14:10:39 +0800

    3.0

---
+++ Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,26 @@
+// 版本一 ，记录当前字母是否出现过？
+class Solution {
+	public:
+		int lengthOfLongestSubstring(string s) {
+
+			int pos[256] = {
+				0};
+			int left = 0;
+			int right = 0;
+			int len = 0;
+			for(size_t i = 0; i < s.size(); ++i){
+				if(pos[s[i]] == 1){
+					while(s[left] != s[i]){
+						pos[s[left]] = 0;
+						++left;
+					}
+					pos[s[left]] = 0;
+					++left;
+				}
+				pos[s[i]] = 1;
+				right = i;
+				len = std::max(len, right - left + 1);
+			}
+			return len;
+		}
+};
