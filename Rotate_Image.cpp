1d4b0b0a4e266c2ee42675f93c6449961221f25d
zhangzhen <1099905725@qq.com>
2016-05-25 22:36:36 +0800

    48.1

--- Rotate_Image.cpp
+++ Rotate_Image.cpp
@@ -17,3 +17,19 @@ class Solution {
             }
         }
 };
+//版本二 ，一次forfor
+class Solution {
+    public:
+        void rotate(vector<vector<int>>& matrix) {
+            const size_t N = matrix.size();
+            for(size_t i = 0; i < N/2; ++i){
+                for(size_t j = i; j < N - i - 1; j++){
+                    int t = matrix[i][j];
+                    matrix[i][j] = matrix[N - 1 - j][i];
+                    matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j];
+                    matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i];
+                    matrix[j][N - 1 - i] = t;
+                }
+            }
+        }
+};
