709bc98f71e865f6d9f3dd2fa0bbc8670f705860
zhangzhen <1099905725@qq.com>
2016-06-05 14:05:09 +0800

    191.1

--- Number_of_1_Bits.cpp
+++ Number_of_1_Bits.cpp
@@ -9,3 +9,14 @@ class Solution {
 			return cnt;
 		}
 };
+
+class Solution {
+	public:
+		int hammingWeight(uint32_t n) {
+			int res = 0;
+			for(int i = 0; i < 32; ++i){
+				res += static_cast<int>(((1 << i) & n) != 0);
+			}
+			return res;
+		}
+};
