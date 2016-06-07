374aeca18cd90e796f16c856853e80dfef5d5871
zhangzhen <1099905725@qq.com>
2016-06-07 16:26:56 +0800

    189.1

--- Rotate_Array.cpp
+++ Rotate_Array.cpp
@@ -1,16 +1,16 @@
 class Solution {
-	public:
-		void rotate(vector<int>& nums, int k) {
-			int n = nums.size();
-			k %= n;
-			if(k == 0) return ;
-			_t(nums, 0, n - 1);
-			_t(nums, 0, k - 1);
-			_t(nums, k, n - 1);
-		}
-		void _t(vector<int>& nums, int start, int end){
-			while(start < end){
-				std::swap(nums[start++], nums[end--]);
-			}
-		}
+    public:
+        void rotate(vector<int>& nums, int k) {
+            int n = nums.size();
+            k %= n;
+            if(k == 0) return ;
+            _t(nums, 0, n - 1);
+            _t(nums, 0, k - 1);
+            _t(nums, k, n - 1);
+        }
+        void _t(vector<int>& nums, int start, int end){
+            while(start < end){
+                std::swap(nums[start++], nums[end--]);
+            }
+        }
 };
