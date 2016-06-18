d363a1dc926079308b929bc7c984bddedf8b6454
zhangzhen <1099905725@qq.com>
2016-06-18 10:49:28 +0800

    5.1 done

--- Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring.cpp
@@ -17,3 +17,26 @@ class Solution {
             return res;
         }
 };
+
+class Solution {
+    public:
+        string longestPalindrome(string s) {
+            string res;
+            int pos = 0, len = 0;
+            for(size_t i = 0; i < s.size(); ++i){
+                int left = i - 1;
+                while(i + 1 < s.size() && s[i + 1] == s[i]) ++i;
+                int right = i + 1;
+                while(left >= 0 && right < s.size() && s[left] == s[right]){
+                    --left;
+                    ++right;
+                }
+                if(right - left - 1 > len){
+                    len = right - left -1;
+                    pos = left + 1;
+                }
+            }
+            res = s.substr(pos, len);
+            return res;
+        }
+};
